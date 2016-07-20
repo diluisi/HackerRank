3e2da824ad04ce5d89998f58f589dc64e75f214e
diluisi <luiz.diego5@gmail.com>
2016-07-20 20:27:29 -0300

    30 days of Code
    30 days of Code

---
+++ 30daysCode/day6.cpp
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+
+int main(){
+    string letra;
+    printf("Digite uma letra: ");
+    letra = getchar();
+    printf("%c", letra);
+}
